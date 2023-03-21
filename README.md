91a976fb0442a13989925f9b209db09f655f73c6
notVlad1997 <92084641+notVlad1997@users.noreply.github.com>
2023-03-21 21:15:07 +0200

    update-1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odd-even
+https://docs.google.com/document/d/13el0kDdzot1MbCU23qf1LuYiPsjl7f0p65N4L07t-U8/edit?usp=sharing
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Python Program to implement
 # Odd-Even / Brick Sort
+from numpy import random
+from memory_profiler import profile
+import time
 
+
+#@profile
 def oddEvenSort(arr, n):
     isSorted = 0
     while isSorted == 0:
@@ -17,9 +22,16 @@ def oddEvenSort(arr, n):
                 isSorted = 0
 
 
-arr = [34, 2, 10, -9]
-n = len(arr)
-
-oddEvenSort(arr, n);
-for i in range(0, n):
-    print(arr[i], end=' ')
+n = 1
+random.seed(169)
+arr = [0]
+for j in range(7, 17):
+    size_of = n * j
+    arr = random.randint(10000000, size=pow(2, j))
+    start = time.time()
+    oddEvenSort(arr, pow(2, j))
+    end = time.time()
+    print(f'{j}:{end - start} secunde')
+    # for i in range(0, pow(2, j)):
+    #     print(arr[i], end=' ')
+    #print()
